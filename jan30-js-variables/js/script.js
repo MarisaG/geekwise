795967f0cdd65ddbb4a5a5385f67e4087e0a77de
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-01-30 15:50:32 -0800

    jan30-js-variables

---
+++ jan30-js-variables/js/script.js
@@ -0,0 +1,21 @@
+//alert("this is an alert");
+
+
+var myNumberVar = 7; //Number Object
+var myFloatVar = 5.2; //Float Object
+var myStringVar = "Marisa Gonzalez"; //string Object
+var myBooleanVar = true; //Bootlean Object
+var myArrayVar = ["one", "stingrey", "banana",8, false]; //array object
+var myObjectVar = {
+  make:"jeep",
+  model:"cute",
+  color:"pink",
+  year:1948
+};//object object
+
+console.log( typeof myNumberVar);
+console.log(typeof myFloatVar);
+console.log( typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myArrayVar);
+console.log(typeof myObjectVar.model);
