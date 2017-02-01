a3c6f63faa2dc6ccc782df93bb598d2c4cf2133b
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-02-01 15:51:30 -0800

    feb01-js-math

---
+++ feb01-js-math/js/script.js
@@ -0,0 +1,31 @@
+var myStringVar="marisa";
+var myNumberVar=2;
+var myFloatVar=5.5;
+var myBooleanVar=false;
+var myArrayVar=["apple","oranages"];
+var myObject={
+  model:"jeep",
+  color:"pink"
+
+};
+console.log(typeof myStringVar);
+console.log(typeof myFloatVar);
+console.log(typeof myNumberVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myObjectVar);
+console.log(typeof myArrayVar);
+
+
+var numUno = 7;
+var numTwo = 4;
+var equalAdded = (numUno + numTwo);
+var equalSub = (numTwo - numUno);
+var equalDiv = (numTwo / numUno);
+var equalMult = numTwo * numUno;
+var equalModulus = numUno % numTwo;//finding even and odd
+
+console.log(equalAdded);
+console.log(equalSub);
+console.log(equalDiv);
+console.log(equalMult);
+console.log(equalModulus);
