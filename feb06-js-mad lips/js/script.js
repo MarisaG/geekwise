2d18888b9ca24d16074ba2ee516cf549adaa7dcc
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-02-07 09:37:02 -0800

    feb06-js-mad lips

---
+++ feb06-js-mad lips/js/script.js
@@ -0,0 +1,26 @@
+var userName = prompt("What is your name?");
+
+var intro = userName + "," + " " + "youre walking in the woods.Theres no one around.And your phones dead. Out of the corner of your eye you spot him... Shia LaBeauf"
+alert(intro)
+
+
+  var userknow = confirm("Do you run or do you fight?");
+  if(userknow==true){
+    alert("You run the oppisote direction");
+  }else{
+    alert("Hes too strong; you have fight to get free and run north");
+  }
+
+  var yourRun = userName + "... " + "hes following you! About 30 ft back. He gets down on all four and breaks into a sprint, hes gaining on you. ... Shia LaBeauf"
+alert(yourRun)
+
+var directionA = "Youre looking for your car but youre all turned around. Hes almost upon you now and you can see theres blood on his face! My god, theres blood everywhere!"
+alert(directionA)
+
+var directionL = confirm ("do you run east or west")
+if(directionL=+true){
+  alert("youve run into thick forest")
+}
+else{
+  alert("yove run into a field")
+}
---
+++ feb06-js-numbers/js/script.js
@@ -0,0 +1,69 @@
+var myNumbervar = 6;
+var myFloatvar = 5.5;
+var myBoolean = false;
+var myObject = {
+  color:"pink",
+  model:"jeep"
+};
+var myArray= ["red", 4, ""]
+var myString="This is a string"
+
+
+// console.log(myString.indexOf("i", 3));
+
+
+var numOne = 2;
+var numTwo = new Number(5);
+
+// console.log(numOne, numTwo);
+
+var numThree = "3";
+var numFour = '5.2';
+// console.log(parseInt(numThree) );
+// console.log(parseInt(numFour) );
+
+
+var numFive = "Haley";
+// console.log(parseInt(numFive));
+//
+// if(isNaN(numFive)){
+// alert("Somebody once told me the World was going to roll me");
+//
+// }
+
+// parseInt
+// parseFloat
+// isNaN
+
+// alert("wha");
+// var userLang = confirm("Do you speak it?");
+// console.log(userLang);
+// var formula = prompt("what is your favorite flavor of raviloi");
+//
+// console.log(formula);
+
+// var firstName = prompt('what is your first name?')
+// var bornyear = prompt('what year were you born in')
+// var favColor = prompt('what is your favorite color?')
+//
+// var sent = "My name is" + " " + firstName + " " + "I was born in"+ " " + bornyear +" " + "and my favorite color is"
+// + " " + favColor
+//
+// console.log(sent);
+
+// var yourAge = prompt('what is your age');
+//
+// console.log(parseInt(yourAge`);
+
+var userResp + confirm("hey you boy?");
+if(userResp){
+  alert("yay u boy");
+  var gender ="boy";
+}
+else{
+  alert("youre  a girl yhen");
+  var gender="girl";
+}
+
+var userGender = "welcome young" + gender;
+alert(userGender);
