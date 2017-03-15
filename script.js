58eb31171371fe4480af434e0747f097dda81704
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-03-15 15:55:27 -0700

    mar15-js-portfolio

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var sky = document.querySelector('header'),
     input = document.querySelector('input[type="color"]'),
     btn = document.querySelector('input[type="submit"]'),
-    bnnr = document.querySelectorAll('.banner')
+    bnnr = document.getElementById('banner'),
+    banner = document.getElementById('bannerTwo')
 
     btn.addEventListener('click', function(e){
       e.preventDefault();
       var skyColor = input.value;
+      var bnnrColor = input.value;
+      var bannerColor = input.value;
       sky.style.backgroundColor = skyColor;
-      bnnr.style.backgroundColor = skyColor
+      bnnr.style.backgroundColor = bnnrColor;
+      banner.style.backgroundColor = bannerColor
 
     });
